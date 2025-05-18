1073c7daa72ef8884c86201a3e3838b4cecc7d5b
parkerstovall <parker.stovall@gmail.com>
2025-05-18 18:13:00 -0500

    working on level one

--- src/game-code/mustachio/levels/level-helpers.ts
+++ src/game-code/mustachio/levels/level-helpers.ts
@@ -1,6 +1,7 @@
 import { BLOCK_SIZE } from '@/game-code/shared/constants'
 import { Wall } from '../classes/game-objects/set-pieces/obstacles/blocks/wall'
 import type { GameContext } from '@/game-code/shared/game-context'
+import { Coin } from '../classes/game-objects/point-objects/items/coin'
 
 export function createWall(
   gameContext: GameContext,
@@ -11,9 +12,39 @@ export function createWall(
 ) {
   for (let i = 0; i < numBlocksWide; i++) {
     for (let j = 0; j < numBlocksHigh; j++) {
-      const x = startingX + i * BLOCK_SIZE
-      const y = startingY + j * BLOCK_SIZE
+      const x = (startingX + i) * BLOCK_SIZE
+      const y = (startingY + j) * BLOCK_SIZE
       gameContext.addGameObject(new Wall(gameContext, x, y))
     }
+  }
+}
+
+export function createCoinWall(
+  gameContext: GameContext,
+  startingX: number,
+  startingY: number,
+  numBlocksWide: number,
+  numBlocksHigh: number,
+) {
+  for (let i = 0; i < numBlocksWide; i += 2) {
+    for (let j = 0; j < numBlocksHigh; j += 2) {
+      const x = (startingX + i) * BLOCK_SIZE
+      const y = (startingY + j) * BLOCK_SIZE
+      gameContext.addGameObject(new Coin(gameContext, x, y))
+    }
+  }
+}
+
+export function createPyramid(
+  gameContext: GameContext,
+  startingX: number,
+  startingY: number,
+  numBlocksWide: number,
+) {
+  while (numBlocksWide > 0) {
+    createWall(gameContext, startingX, startingY, numBlocksWide, 1)
+    startingX += 1
+    startingY -= 1
+    numBlocksWide -= 2
   }
 }
--- src/game-code/mustachio/levels/level-one.ts
+++ src/game-code/mustachio/levels/level-one.ts
@@ -1,22 +1,65 @@
 import { GameContext } from '@/game-code/shared/game-context'
-import { createWall } from './level-helpers'
+import { createCoinWall, createPyramid, createWall } from './level-helpers'
 import { BLOCK_SIZE } from '@/game-code/shared/constants'
 import { Floor } from '../classes/game-objects/set-pieces/obstacles/floor'
+import { ItemBlock } from '../classes/game-objects/set-pieces/obstacles/blocks/punchable-blockS/item-block'
+import { Brick } from '../classes/game-objects/set-pieces/obstacles/blocks/punchable-blockS/brick'
+import { Wall } from '../classes/game-objects/set-pieces/obstacles/blocks/wall'
+import { StacheStalker } from '../classes/game-objects/point-objects/enemies/stache-stalker'
 
 export function levelOne(gameContext: GameContext) {
   gameContext.clearLevel()
-
-  // First barrier
-  createWall(gameContext, 0, 0, 1, 18)
 
   // Floor
   gameContext.addGameObject(
     new Floor(gameContext, {
       x: 0,
       y: BLOCK_SIZE * 17,
-      width: BLOCK_SIZE * 32,
+      width: BLOCK_SIZE * 40,
       height: BLOCK_SIZE,
     }),
+  )
+
+  // First barrier
+  createWall(gameContext, 0, 0, 1, 17)
+  createCoinWall(gameContext, 1, 5, 1, 11)
+
+  gameContext.addGameObject(
+    new ItemBlock(gameContext, BLOCK_SIZE * 6, BLOCK_SIZE * 14, false, 'coin'),
+  )
+
+  gameContext.addGameObject(
+    new Brick(gameContext, BLOCK_SIZE * 7, BLOCK_SIZE * 14),
+  )
+
+  gameContext.addGameObject(
+    new ItemBlock(gameContext, BLOCK_SIZE * 8, BLOCK_SIZE * 14, true, 'coin'),
+  )
+
+  gameContext.addGameObject(
+    new ItemBlock(gameContext, BLOCK_SIZE * 8, BLOCK_SIZE * 11, true, 'coin'),
+  )
+
+  gameContext.addGameObject(
+    new Brick(gameContext, BLOCK_SIZE * 9, BLOCK_SIZE * 14),
+  )
+
+  gameContext.addGameObject(
+    new ItemBlock(gameContext, BLOCK_SIZE * 10, BLOCK_SIZE * 14, false, 'coin'),
+  )
+
+  createPyramid(gameContext, 16, 16, 11)
+
+  gameContext.addGameObject(
+    new Wall(gameContext, BLOCK_SIZE * 39, BLOCK_SIZE * 16),
+  )
+
+  gameContext.addGameObject(
+    new StacheStalker(gameContext, BLOCK_SIZE * 28, BLOCK_SIZE * 14),
+  )
+
+  gameContext.addGameObject(
+    new StacheStalker(gameContext, BLOCK_SIZE * 37.5, BLOCK_SIZE * 14),
   )
 
   gameContext.startMainLoop()
