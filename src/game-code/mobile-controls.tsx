0db4b76a1067f181b342dbbb842801e6a41dc5a4
parkerstovall <parker.stovall@gmail.com>
2025-06-05 08:59:44 -0500

    event name fix (#18)

--- src/game-code/mobile-controls.tsx
+++ src/game-code/mobile-controls.tsx
@@ -1,11 +1,11 @@
 export function MobileControls() {
   // The buttons just translate to keydowns
-  const onMouseDown = (key: string) => {
+  const onTouchStart = (key: string) => {
     const event = new KeyboardEvent('keydown', { key })
     window.dispatchEvent(event)
   }
 
-  const onMouseUp = (key: string) => {
+  const onTouchEnd = (key: string) => {
     const event = new KeyboardEvent('keyup', { key })
     window.dispatchEvent(event)
   }
@@ -13,23 +13,23 @@ export function MobileControls() {
   return (
     <div className="mobile-controls gameCanvas">
       <button
-        onMouseDown={() => onMouseDown('ArrowLeft')}
-        onMouseUp={() => onMouseUp('ArrowLeft')}
+        onTouchStart={() => onTouchStart('ArrowLeft')}
+        onTouchEnd={() => onTouchEnd('ArrowLeft')}
         className="left"
       ></button>
       <button
-        onMouseDown={() => onMouseDown('ArrowRight')}
-        onMouseUp={() => onMouseUp('ArrowRight')}
+        onTouchStart={() => onTouchStart('ArrowRight')}
+        onTouchEnd={() => onTouchEnd('ArrowRight')}
         className="right"
       ></button>
       <button
-        onMouseDown={() => onMouseDown('ArrowUp')}
-        onMouseUp={() => onMouseUp('ArrowUp')}
+        onTouchStart={() => onTouchStart('ArrowUp')}
+        onTouchEnd={() => onTouchEnd('ArrowUp')}
         className="jump"
       ></button>
       <button
-        onMouseDown={() => onMouseDown(' ')}
-        onMouseUp={() => onMouseUp(' ')}
+        onTouchStart={() => onTouchStart(' ')}
+        onTouchEnd={() => onTouchEnd(' ')}
         className="fire"
       ></button>
     </div>
