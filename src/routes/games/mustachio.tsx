425aad6dbf0b296aebe3d55183a9396716940d97
parkerstovall <parker.stovall@gmail.com>
2026-01-30 09:39:00 -0600

    separate mustachio into an npm package

--- src/routes/games/mustachio.tsx
+++ src/routes/games/mustachio.tsx
@@ -1,33 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useEffect } from 'react'
-import { MobileControls } from '@/game-code/mobile-controls'
-import { levelOne } from '@/game-code/custom/mustachio/levels/level-one'
-import { MustachioGameContext } from '@/game-code/custom/mustachio/mustachi-game-context'
+import { startMustachio } from 'mustachio-game'
 
 export const Route = createFileRoute('/games/mustachio')({
   component: MustachioGame,
 })
 
-let gc: MustachioGameContext | null = null
-
 function MustachioGame() {
   useEffect(() => {
-    if (!gc) {
-      gc = new MustachioGameContext()
-      gc.restart(levelOne)
-    }
+    startMustachio('game-container')
   }, [])
 
-  const useMobileControls = window.innerWidth <= 1000
+  // const useMobileControls = window.innerWidth <= 1000
 
   return (
     <div>
       <h1>Mustachio</h1>
       <p>Welcome to the Mustachio game!</p>
-      <canvas id="background-layer" className="gameCanvas" />
-      <canvas id="game-layer" className="gameCanvas" />
-      <canvas id="ui-layer" className="gameCanvas" />
-      {useMobileControls && <MobileControls />}
+      <div id="game-container"></div>
+      {/* {useMobileControls && <MobileControls />} */}
     </div>
   )
 }
